78fd32f4004ef1b36c56e9bacd128dff0b98fcf1
raimelee <106028986+raimelee@users.noreply.github.com>
2022-06-01 16:23:46 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,52 @@
+/*
+
+1: PROMPT user for name, store in variable
+2: PTOMPT: 3 holes or 6, store in variable
+
+If 3 then 3 PROMPTS, if 6 then 6 PROMPTS
+3: LOOP: holeCount (3 or 6)
+    ask for score
+    add that to total
+    
+
+4: Print total
+5: If (total <= )
+    print AAA
+else
+    print BBB
+
+
+*/
+
+document.write(`Mini Golf`);
+console.log(`Mini Golf`);
+console.warn(`Winner Takes All!`);
+
+let parThree = 9
+let parSix = 18
+
+
+let myName = prompt(`Welcome to Mini Golf! What's your name?`);
+alert(`Hello, ${myName}! Let's play!`);
+console.log(`Hello ${myName}! Let's play!`);
+
+let holeCount = prompt('How many holes are you playing today: 3 or 6?')
+let totalScore = 0
+for (let i = 0; i < holeCount; i++) {
+    let putts = Number(prompt(`Enter putt count for hole ` + (i + 1)));
+    totalScore += putts;
+}
+
+if (holeCount == 3 && totalScore <= parThree) {
+    alert(`Great job, ${myName}! Your total par was: ${totalScore}`)
+    console.log(`Great job, ${myName}! Your total par was: ${totalScore}`)
+} else if (holeCount == 3 && totalScore > parThree) {
+    alert(`Nice try, ${myName}! Your total par was: ${totalScore}`)
+    console.log(`Nice try, ${myName}! Your total par was: ${totalScore}`)
+} else if (holeCount == 6 && totalScore <= parSix) {
+    alert(`Great job, ${myName}! Your total par was: ${totalScore}`)
+    console.log(`Great job, ${myName}! Your total par was: ${totalScore}`)
+} else if (holeCount == 6 && totalScore > parSix) {
+    alert(`Nice try, ${myName}! Your total par was: ${totalScore}`)
+    console.log(`Nice try, ${myName}! Your total par was: ${totalScore}`)
+}
